980fdccc16491749353dcce975872790ce674957
abdulkareemm <eng.kreem.96@gmail.com>
2022-09-19 11:14:56 -0700

    Community component

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
+import {community} from "../data"
+
+import {CommunitySlider}from "./index"
 const Community = () => {
-  return <div>Community</div>;
+  const {icon,title} = community
+  return (
+    <section className="section relative">
+      <div className="container mx-auto">
+        <div className="flex">
+          {/* section title */}
+
+          <div
+            className="section-title-group max-w-[240px] mx-auto px-4 lg:px-0 lg:ml-0 "
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay="200"
+          >
+            <img src={icon} alt="pricing icon" />
+            <h2 className="h2 section-title">
+              {title}
+              <span className="text-primary-200">.</span>
+            </h2>
+          </div>
+          <div
+            className="absolute -right-[375px] lg:-right-[280px] w-[1140px] top-48 lg:top-0"
+            data-aos="fade-left"
+            data-aos-offset="200"
+            data-aos-delay="500"
+          >
+            <CommunitySlider />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Community;
--- src/components/CommunitySlider.jsx
+++ src/components/CommunitySlider.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
+import {community}from "../data"
+// import swiper react component
+import {Swiper,SwiperSlide} from 'swiper/react'
+
+// import swiper style 
+import "swiper/css"
+import "swiper/css/navigation"
+import "../workoutSlider.css"
+
+// import require modules
+import {Navigation} from "swiper"
+
 const CommunitySlider = () => {
-  return <div>CommunitySlider</div>;
+  const {testimonials} = community
+  return <Swiper slidesPerView={3}
+  spaceBetween={32}
+  centeredSlides={'auto'}
+  grabCursor={true}
+  modules={[Navigation]}
+  loop={true}
+  >
+  {testimonials.map((slider,index)=>{
+    const {image,message,name} = slider
+    return (
+      <SwiperSlide key={index}>
+        <div className="relative">
+          <div>
+            <img src={image} alt="" />
+          </div>
+          <div className='absolute bottom-[30px] text-white p-[20px] text-center'>
+            <div className='mb-8 italic text-lg font-light'>{message}</div>
+            <div className="flex items-center justify-center gap-x-[3px]">
+              <span className="text-[30px] text-primary-200 font-bold">~</span>
+              <div className="text-[20px] font-bold">
+                {name}
+              </div>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+    );
+  })}
+  </Swiper>;
 };
 
 export default CommunitySlider;
