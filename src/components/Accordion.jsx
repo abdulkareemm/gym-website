24c6936949a23069eb911e416da0d21191469530
abdulkareemm <eng.kreem.96@gmail.com>
2022-09-19 19:10:41 -0700

    Faq component

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Accordion = () => {
-  return <div>Accordion</div>;
+import { FaChevronCircleUp, FaChevronCircleDown } from "react-icons/fa";
+const Accordion = ({ accordion }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { question, answer } = accordion;
+  return (
+    <div className="cursor-pointer" onClick={()=>setIsOpen(!isOpen)}>
+      <div className="bg-white border rounded-sm">
+        <div className="min-h-[60px] flex items-center justify-between px-[30px]">
+          <h6 className="h6">{question}</h6>
+          <div>
+            {isOpen ? (
+              <FaChevronCircleUp className="text-[20px] text-neutral-500" />
+            ) : (
+              <FaChevronCircleDown className="text-[20px] text-neutral-500" />
+            )}
+          </div>
+        </div>
+        <div className={`${isOpen?'min-h-[200px] lg:min-h-[160px]':'min-h-0'} max-h-0 overflow-hidden flex justify-center transition-all px-[30px]`}>
+          <div className="mt-6">
+            {answer}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Accordion;
--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from "react";
 
+import { faq } from "../data";
+
+import {Accordion} from "./index"
+
+import {} from "react-icons"
 const Faq = () => {
-  return <div>Faq</div>;
+
+  const {accordions,icon,title} = faq
+  return <section className="section pt-[480px] lg:pt-[280px]">
+  <div className="max-w-[768px] mx-auto lg:bg-faq bg-no-repeat bg-custom bg-center lg:h-[1160px] lg:pt-6" data-aos="fade-up"
+    data-aos-offset = "300"
+    data-aos-delay="200">
+    <div className="section-title-group justify-start lg:justify-center -space-x-4 lg:max-w-[540px] mx-auto px-4 lg:px-0 ">
+      <img src={icon} alt="" className="lg:hidden"/>
+      <h2 className="h2 section-title lg:mt-[100px]">{title} <span className="text-peimary-200">.</span></h2>
+    </div>
+    <div className="flex flex-col gap-y-4 px-4 "
+    data-aos="fade-up"
+    data-aos-offset = "300"
+    data-aos-delay="200">
+      {accordions.map((accordion,index)=>{
+        return <Accordion accordion={accordion} key={index}/ >
+
+      })}
+    </div>
+  </div>
+  </section>;
 };
 
 export default Faq;
